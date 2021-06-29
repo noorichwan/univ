066cfa86613256b675e31f17109f235487033296
noorichwan <muhammadnoorichwan@gmail.com>
2021-06-29 12:52:31 +0800

    Sudah Selesai Penambahan Edit, hapus, dan tambah data

---
+++ app/Http/Controllers/MakulController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Makul;
+use Illuminate\Http\Request;
+Use Alert;
+
+class MakulController extends Controller
+{
+    public function index()
+    {
+        $makul = Makul::all();//select * from nama_table
+        return view('makul.index', compact('makul'));
+    }    
+
+    public function create()
+    {
+        $makul = Makul::all();
+        return view('makul.create', compact('makul'));
+    }
+
+    public function store(Request $request)
+    {
+        Makul::create($request->all());
+        alert()->success('Sukses','Data Berhasil Disimpan');
+        return redirect()->route('makul');
+    }
+
+    public function edit($id)
+    {
+        $makul = Makul::find($id);
+        return view('makul.edit', compact('makul'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $makul = Makul::find($id);
+        $makul->update($request->all());
+        toast('Data Berhasil Diedit','success');
+        return redirect()->route('makul');
+    }
+
+    public function destroy($id)
+    {
+        $makul = Makul::find($id);
+        $makul->delete();
+        toast('Data Berhasil Dihapus','success');
+        return redirect()->route('makul');
+    }
+}
+
